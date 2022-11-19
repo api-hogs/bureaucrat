91c53ba4ca6ec762711842416fa93a4ae5dd8033
Alexandre Costa <alexandremcost@gmail.com>
2022-11-19 17:30:08 -0300

    fix test module name

--- test/postman_writer_test.exs
+++ test/postman_writer_test.exs
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 defmodule Bureaucrat.PostmanWriterTest do
   use ExUnit.Case
 
   describe "postman writer test" do
-=======
-defmodule Bureaucrat.ApiBlueprintWriterTest do
-  use ExUnit.Case
-
-  describe "api blueprint writer test" do
->>>>>>> 91ee819 (implement postman writer)
   end
 end
