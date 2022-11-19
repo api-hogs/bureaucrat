1b9d50db6ffbf04edd24a74b0d044cf4a714cde1
Alexandre Costa <alexandremcost@gmail.com>
2022-11-19 17:27:25 -0300

    implement postman writer

--- test/postman_writer_test.exs
+++ test/postman_writer_test.exs
@@ -1,6 +1,13 @@
+<<<<<<< HEAD
 defmodule Bureaucrat.PostmanWriterTest do
   use ExUnit.Case
 
   describe "postman writer test" do
+=======
+defmodule Bureaucrat.ApiBlueprintWriterTest do
+  use ExUnit.Case
+
+  describe "api blueprint writer test" do
+>>>>>>> 91ee819 (implement postman writer)
   end
 end
