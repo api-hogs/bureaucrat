b22f95c495ef6254f1f97dd529be89a2d44df3ba
Andy Tran <andy8tran@gmail.com>
2023-03-06 11:41:50 -0500

    Change to """ messages

--- lib/bureaucrat/helpers.ex
+++ lib/bureaucrat/helpers.ex
@@ -133,12 +133,15 @@ defmodule Bureaucrat.Helpers do
 
   def format_test_name("test " <> name), do: name
 
-  def format_test_name(function_name),
-    do: raise("It looks like you called a `Phoenix.ConnTest` macro inside `#{function_name}`.
-        Bureaucrat can only document macros `get`, `post`, `delete`, etc. when they are called inside a `test` block.
+  def format_test_name(function_name) do
+    raise """
+    It looks like you called a `Phoenix.ConnTest` macro inside `#{function_name}`.
+    Bureaucrat can only document macros `get`, `post`, `delete`, etc. when they are called inside a `test` block.
 
-        If the request macro is called inside a private function or setup, you should explicitly say you don't want Bureaucrat to document this request.
-        Use `get_undocumented`, `post_undocumented`, `delete_undocumented`, `patch_undocumented` or `put_undocumented` instead.")
+    If the request macro is called inside a private function or setup, you should explicitly say you don't want Bureaucrat to document this request.
+    Use `get_undocumented`, `post_undocumented`, `delete_undocumented`, `patch_undocumented` or `put_undocumented` instead.
+    """
+  end
 
   def group_title_for(_mod, []), do: nil
 
@@ -157,9 +160,11 @@ defmodule Bureaucrat.Helpers do
         "#{controller}.#{action}"
 
       _ ->
-        raise "Bureaucrat couldn't find a controller and/or action for this request.
-            Possibly, the request is halted by a plug before it gets to the controller.
-            Please use `get_undocumented` or `post_undocumented` (etc.) instead."
+        raise """
+        Bureaucrat couldn't find a controller and/or action for this request.
+        Possibly, the request is halted by a plug before it gets to the controller.
+        Please use `get_undocumented` or `post_undocumented` (etc.) instead.
+        """
     end
   end
 
